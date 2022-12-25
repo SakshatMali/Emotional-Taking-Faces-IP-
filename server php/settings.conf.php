7fc516c4602a85d31c48a6780ec51948565f96e2
Sakshat Mali <55681999+SakshatMali@users.noreply.github.com>
2022-12-25 19:31:13 +0530

    server files

---
+++ server php/settings.conf.php
@@ -0,0 +1,6 @@
+<?php
+// The URL of the backend server that browser requests should be proxied to
+$CMS_SERVERHOST = 'http://192.168.23.36:8080';
+
+// Max times to follow a HTTP redirection response to a new URL
+$HTTP_MAX_REDIRECTS = 10;
